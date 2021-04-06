0b9e62e59eb1f17208b40e598b5122a42efb544e
Marco Zuniga Ugalde <mazuga92@gmail.com>
2021-04-06 02:14:08 +0000

    final todo list api

--- src/app.py
+++ src/app.py
@@ -1,6 +1,35 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+import json
 app = Flask(__name__)
 
+todos = [
+    {
+        "label": "My first task",
+        "done": True 
+    },
+    {
+        "label": "My second task",
+         "done": True 
+    }
+]
+
+@app.route('/todos', methods=['GET'])
+def hello_world():    
+    return jsonify(todos)
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decoded = json.loads(request_body)
+    todos.append(decoded)
+    return jsonify(todos)
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    return jsonify(todos)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3245, debug=True)
+
+
